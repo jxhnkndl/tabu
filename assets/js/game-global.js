5b7dce39cbdeaa4371db942ad6960ca5dd17873d
jxhnkndl <jkroyston@gmail.com>
2025-02-13 13:52:58 -0500

    update properties on main gameboard array elements

--- assets/js/game-global.js
+++ assets/js/game-global.js
@@ -28,260 +28,335 @@ const gameboard = [
     {
       id: 'cell-1a',
       row: 1, 
+      colorFamily: 'green',
       alt: 'Pentagram',
       dark: './assets/images/symbols-dark/pentagram-dark.svg',
       color: './assets/images/symbols-green/pentagram-green.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pentagram-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: greenStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-1b',
       row: 1,
+      colorFamily: 'purple',
       alt: 'Fingerprint',
       dark: './assets/images/symbols-dark/fingerprint-dark.svg',
       color: './assets/images/symbols-purple/fingerprint-purple.svg',
       winningColor: `./assets/images/symbols-${winningColor}/fingerprint-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: purpleStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-1c',
       row: 1,
+      colorFamily: 'yellow',
       alt: 'Void',
       dark: './assets/images/symbols-dark/void-dark.svg',
       color: './assets/images/symbols-yellow/void-yellow.svg',
       winningColor: `./assets/images/symbols-${winningColor}/void-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: yellowStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-1d',
       row: 1,
+      colorFamily: 'orange',
       alt: 'Pyramid',
       dark: './assets/images/symbols-dark/pyramid-dark.svg',
       color: './assets/images/symbols-orange/pyramid-orange.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: orangeStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-1e',
       row: 1,
+      colorFamily: 'blue',
       alt: 'Inverted Pyramid',
       dark: './assets/images/symbols-dark/inverted-pyramid-dark.svg',
       color: './assets/images/symbols-blue/inverted-pyramid-blue.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: blueStatusEl,
+      revealed: false,
+      remaining: 5
     },    
   ],
   [
     {
       id: 'cell-2a',
       row: 2, 
+      colorFamily: 'blue',
       alt: 'Fingerprint',
       dark: './assets/images/symbols-dark/fingerprint-dark.svg',
       color: './assets/images/symbols-blue/fingerprint-blue.svg',
       winningColor: `./assets/images/symbols-${winningColor}/fingerprint-${winningColor}.svg`,
       letter: './assets/images/letters/letter-t-yellow.svg',
-      revealed: false
+      statusBar: blueStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-2b',
       row: 2,
+      colorFamily: 'orange',
       alt: 'Pentagram',
       dark: './assets/images/symbols-dark/pentagram-dark.svg',
       color: './assets/images/symbols-orange/pentagram-orange.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pentagram-${winningColor}.svg`,
       letter: './assets/images/letters/letter-a-yellow.svg',
-      revealed: false
+      statusBar: orangeStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-2c',
       row: 2,
+      colorFamily: 'green',
       alt: 'Pyramid',
       dark: './assets/images/symbols-dark/pyramid-dark.svg',
       color: './assets/images/symbols-green/pyramid-green.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pyramid-${winningColor}.svg`,
       letter: './assets/images/letters/letter-p-yellow.svg',
-      revealed: false
+      statusBar: greenStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-2d',
       row: 2,
+      colorFamily: 'yellow',
       alt: 'Inverted Pyramid',
       dark: './assets/images/symbols-dark/inverted-pyramid-dark.svg',
       color: './assets/images/symbols-yellow/inverted-pyramid-yellow.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: yellowStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-2e',
       row: 2,
+      colorFamily: 'purple',
       alt: 'Void',
       dark: './assets/images/symbols-dark/void-dark.svg',
       color: './assets/images/symbols-purple/void-purple.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: purpleStatusEl,
+      revealed: false,
+      remaining: 5
     },  
   ],
   [
     {
       id: 'cell-3a',
       row: 3, 
+      colorFamily: 'orange',
       alt: 'Void',
       dark: './assets/images/symbols-dark/void-dark.svg',
       color: './assets/images/symbols-orange/void-orange.svg',
       winningColor: `./assets/images/symbols-${winningColor}/void-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: orangeStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-3b',
       row: 3,
+      colorFamily: 'orange',
       alt: 'Inverted Pyramid',
       dark: './assets/images/symbols-dark/inverted-pyramid-dark.svg',
       color: './assets/images/symbols-orange/inverted-pyramid-orange.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: orangeStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-3c',
       row: 3,
+      colorFamily: 'purple',
       alt: 'Pyramid',
       dark: './assets/images/symbols-dark/pyramid-dark.svg',
       color: './assets/images/symbols-purple/pyramid-purple.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pyramid-${winningColor}.svg`,
       letter: './assets/images/letters/letter-t-yellow.svg',
-      revealed: false
+      statusBar: purpleStatusEl,
+      revealed: false,
+      remaining: 4
     },
     {
       id: 'cell-3d',
       row: 3,
+      colorFamily: 'green',
       alt: 'Fingerprint',
       dark: './assets/images/symbols-dark/fingerprint-dark.svg',
       color: './assets/images/symbols-green/fingerprint-green.svg',
       winningColor: `./assets/images/symbols-${winningColor}/fingerprint-${winningColor}.svg`,
       letter: './assets/images/letters/letter-o-yellow.svg',
-      revealed: false
+      statusBar: greenStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-3e',
       row: 3,
+      colorFamily: 'blue',
       alt: 'Pentagram',
       dark: './assets/images/symbols-dark/pentagram-dark.svg',
       color: './assets/images/symbols-blue/pentagram-blue.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pentagram-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: blueStatusEl,
+      revealed: false,
+      remaining: 5
     },  
   ],
   [
     {
       id: 'cell-4a',
       row: 4, 
+      colorFamily: 'yellow',
       alt: 'Pyramid',
       dark: './assets/images/symbols-dark/pyramid-dark.svg',
       color: './assets/images/symbols-yellow/inverted-pyramid-yellow.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: yellowStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-4b',
       row: 4,
+      colorFamily: 'orange',
       alt: 'Fingerprint',
       dark: './assets/images/symbols-dark/fingerprint-dark.svg',
       color: './assets/images/symbols-orange/fingerprint-orange.svg',
       winningColor: `./assets/images/symbols-${winningColor}/fingerprint-${winningColor}.svg`,
       letter: './assets/images/letters/letter-p-yellow.svg',
-      revealed: false
+      statusBar: orangeStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-4c',
       row: 4,
+      colorFamily: 'blue',
       alt: 'Void',
       dark: './assets/images/symbols-dark/void-dark.svg',
       color: './assets/images/symbols-blue/void-blue.svg',
       winningColor: `./assets/images/symbols-${winningColor}/void-${winningColor}.svg`,
       letter: './assets/images/letters/letter-l-yellow.svg',
-      revealed: false
+      statusBar: blueStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-4d',
       row: 4,
+      colorFamily: 'purple',
       alt: 'Pentagram',
       dark: './assets/images/symbols-dark/pentagram-dark.svg',
       color: './assets/images/symbols-purple/pentagram-purple.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pentagram-${winningColor}.svg`,
       letter: './assets/images/letters/letter-a-yellow.svg',
-      revealed: false
+      statusBar: purpleStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-4e',
       row: 4,
+      colorFamily: 'green',
       alt: 'Inverted Pyramid',
       dark: './assets/images/symbols-dark/inverted-pyramid-dark.svg',
       color: './assets/images/symbols-green/inverted-pyramid-green.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: './assets/images/letters/letter-y-yellow.svg',
-      revealed: false
+      statusBar: greenStatusEl,
+      revealed: false,
+      remaining: 5
     },  
   ],
   [
     {
       id: 'cell-5a',
       row: 5, 
+      colorFamily: 'purple',
       alt: 'Inverted Pyramid',
       dark: './assets/images/symbols-dark/inverted-pyramid-dark.svg',
       color: './assets/images/symbols-purple/inverted-pyramid-purple.svg',
       winningColor: `./assets/images/symbols-${winningColor}/inverted-pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: purpleStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-5b',
       row: 5,
+      colorFamily: 'blue',
       alt: 'Pyramid',
       dark: './assets/images/symbols-dark/pyramid-dark.svg',
       color: './assets/images/symbols-blue/pyramid-blue.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pyramid-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: blueStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-5c',
       row: 5,
+      colorFamily: 'yellow',
       alt: 'Fingerprint',
       dark: './assets/images/symbols-dark/fingerprint-dark.svg',
       color: './assets/images/symbols-yellow/fingerprint-yellow.svg',
       winningColor: `./assets/images/symbols-${winningColor}/fingerprint-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: yellowStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-5d',
       row: 5,
+      colorFamily: 'green',
       alt: 'Void',
       dark: './assets/images/symbols-dark/void-dark.svg',
       color: './assets/images/symbols-green/void-green.svg',
       winningColor: `./assets/images/symbols-${winningColor}/void-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: greenStatusEl,
+      revealed: false,
+      remaining: 5
     },
     {
       id: 'cell-5e',
       row: 5,
+      colorFamily: 'yellow',
       alt: 'Pentagram',
       dark: './assets/images/symbols-dark/pentagram-dark.svg',
       color: './assets/images/symbols-yellow/pentagram-yellow.svg',
       winningColor: `./assets/images/symbols-${winningColor}/pentagram-${winningColor}.svg`,
       letter: false,
-      revealed: false
+      statusBar: yellowStatusEl,
+      revealed: false,
+      remaining: 5
     },  
   ],
 ];
